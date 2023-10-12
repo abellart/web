a64691f4791f147bad08cbc70212658d381d96d6
abella <annette.2003@mail.ru>
2023-10-12 18:37:06 +0300

    change index.php

--- lw1/index.php
+++ lw1/index.php
@@ -9,18 +9,18 @@ $users = array(
     new User('Катя', '456', '05/05/2007'),
 );
 
-echo 'Сортировка по имени (А-Я)'.PHP_EOL;;
+echo 'Сортировка по имени (А-Я)'.PHP_EOL;
 $usernameAscending = UserService::sortByUsername($users, true);
 print_r($usernameAscending);
 
-echo 'Сортировка по имени (Я-А)'.PHP_EOL;;
+echo 'Сортировка по имени (Я-А)'.PHP_EOL;
 $usernameDescending = UserService::sortByUsername($users, false);
 print_r($usernameDescending);
 
-echo 'Сортировка по дню рождения (<)'.PHP_EOL;;
+echo 'Сортировка по дню рождения (<)'.PHP_EOL;
 $birthdayAscending = UserService::sortByBirthday($users, true);
 print_r($birthdayAscending);
 
-echo 'Сортировка по дню рождения (>)'.PHP_EOL;;
+echo 'Сортировка по дню рождения (>)'.PHP_EOL;
 $birthdayDescending = UserService::sortByBirthday($users, false);
 print_r($birthdayDescending);
